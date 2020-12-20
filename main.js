3a8aef8dc971066ed419f5f29b9bc8579f3740c9
yuusuke16 <murakai1016@gmail.com>
2020-12-20 13:53:57 +0900

    stdy

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 'use strict'
-new Vue({
-  el: "#app",
-  data:{
-  message:'HelloWorld'
+new Vue ({
+  el: '#app',
+  data: {
+    number: 0,
+    x:0,
+    y:0
   },
   methods: {
-  reverseMessage: function(){
-  this.message = this.message.split('').reverse().join('')
+    countUp: function(times){
+      this.number +=1 * times
+    },
+    changeMousePosition:function(divideNumber, event){
+      this.x =event.clientX / divideNumber;
+      this.y =event.clientY / divideNumber;
+
+    }
   }
-  }
-})
+  })
