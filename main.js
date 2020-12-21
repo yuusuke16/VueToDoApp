8730d655efe5b4ced5729ba3506b5d9914fbe4bd
yuusuke16 <murakai1016@gmail.com>
2020-12-21 22:50:48 +0900

    todo

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-new Vue ({
-  el: '#app',
-  data: {
-    isActive:true
-  },
-  computed: {
-    classObject:function(){
-      return {
-        red: this.isActive,
-        'bg-blue': !this.isActive
-      }
+(function() {
+  'use strict';
+
+
+
+  var vm = new Vue({
+    el:'#app',
+    data: {
+      todos:[
+        'task 1',
+        'task 2',
+        'task 3',
+      ]
     }
-  }
-  
-})
+  });
+})();
