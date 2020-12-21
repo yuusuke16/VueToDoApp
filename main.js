7fcb6b4a56c8bc743f36b87a67317ee5d1aa66b2
yuusuke16 <murakai1016@gmail.com>
2020-12-21 22:25:03 +0900

    js

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-'use strict'
 new Vue ({
   el: '#app',
   data: {
-    number: 0,
-    x:0,
-    y:0
+    isActive:true
   },
-  methods: {
-    countUp: function(times){
-      this.number +=1 * times
-    },
-    changeMousePosition:function(divideNumber, event){
-      this.x =event.clientX / divideNumber;
-      this.y =event.clientY / divideNumber;
-
+  computed: {
+    classObject:function(){
+      return {
+        red: this.isActive,
+        'bg-blue': !this.isActive
+      }
     }
   }
-  })
+  
+})
